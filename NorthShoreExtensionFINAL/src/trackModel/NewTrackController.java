527caa2b6f66a4cee3c08ba4c96012bbcfb5b772
sbunke <seb93@pitt.edu>
2013-11-20 18:06:12 -0800

    added private import method

--- NorthShoreExtensionFINAL/src/trackModel/NewTrackController.java
+++ NorthShoreExtensionFINAL/src/trackModel/NewTrackController.java
@@ -94,6 +94,53 @@ public class NewTrackController {
     	
     	
     }
+    
+    public void ImportFile(String filename)
+    {
+    	TrackObject trckOb = new TrackObject();
+    	String trackLineColor=null;
+    	try {
+			Scanner fileScan = new Scanner(new File(filename));
+			
+			
+			String linetwo = fileScan.nextLine();
+			
+			while (fileScan.hasNextLine()) {
+				String line = fileScan.nextLine();
+				String[] blockInfo = line.split(",");
+				
+				trackLineColor=blockInfo[0];
+				trckOb.setLine(blockInfo[0]);
+				Block newBlock = new Block(blockInfo);
+				trckOb.addBlock(newBlock);
+				/*
+				System.out.print(blockInfo[0] + " ");
+				System.out.print(blockInfo[1] + " ");
+				System.out.print(blockInfo[2] + " ");
+				System.out.print(blockInfo[3] + " ");
+				System.out.print(blockInfo[4] + " ");
+				System.out.print(blockInfo[5] + " ");
+				System.out.print(blockInfo[6] + " ");
+				System.out.print(blockInfo[7] + " ");
+				System.out.print(blockInfo[8] + " ");
+				System.out.print(blockInfo[9] + " ");
+				
+				System.out.println();
+				*/
+			}
+			fileScan.close();
+			Track.trackArray.put(trckOb.getLine(), trckOb);
+			Track.trackListData.add(trackLineColor);
+			TrackMainController.trackLineList.setItems(Track.trackListData);
+			Stage stage = (Stage)CreateNewTrackButton.getScene().getWindow();
+	    	stage.close();
+		} 
+		
+		catch (FileNotFoundException e) {
+			FileNotFoundLabel.setVisible(true);
+			
+		}
+    }
 
     @FXML
     void initialize() {
