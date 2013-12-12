28f6c7dbec9aea5881bc071798b0559a222faf3f
Keith Payne <keith1724@gmail.com>
2013-12-11 19:41:28 -0500

    Update CTC track size

--- NorthShoreExtensionFINAL/src/ctc/CTCController.java
+++ NorthShoreExtensionFINAL/src/ctc/CTCController.java
@@ -49,7 +49,7 @@ public class CTCController
 	private String removeTrainForm = "removeTrainForm.fxml";
 	private String routeTrainForm = "routeTrainForm.fxml";
 	private String setpointForm = "suggestSetptForm.fxml";
-	private int shrinkDisplay = 6;
+	private int shrinkDisplay = 9;
 	
 	/**
 	 * Sets the CTC office model that we are controlling
