8ba4132ef032e26e533f0347573041f2f13fad29
brandonbock <me@brandonbock.com>
2013-12-12 17:22:13 -0500

    TrnC bug report

---
+++ NorthShoreExtensionFINAL/src/TrainController/TrainControllerTest.java
@@ -0,0 +1,64 @@
+package TrainController;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TrainControllerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testTrainController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOpenDoors() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCloseDoors() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnOnLights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnOffLights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCruiseControl() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsTrainStopped() {
+		fail("Not yet implemented");
+	}
+
+}
